7fc6df07f65afc5c02d9bf26f69c696dce85a083
bibipearce <b.pearce@corecomtechacademy.co.uk>
2026-04-15 09:14:37 +0100

    adding my O&X files to this repository

---
+++ OandX.js
@@ -0,0 +1,47 @@
+
+//retrieve elements we want to interact with
+const board = document.getElementById('board') 
+const squares = document.getElementsByClassName('squares')
+
+//make players to interact with the game/board
+const players = ['X', 'O']
+let currentPlayer = players[0]
+
+//we need to define how the game can be won and check for that
+const winningCombos = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    [1, 5, 9], [3, 5, 7]
+]
+
+function checkWin(currentPlayer) {
+    for (let i=0; i<winningCombos.length; i++) {
+        const [a, b, c] = winningCombos[i]
+        if (squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer) {
+            return true
+        }
+    }
+    return false
+}
+
+function checkTie() {
+    for (let i=0; i<squares.length; i++) {
+        if (squares[i].textContent === '') {
+            return message.textContent = "X's turn!";
+            currentPlayer = players[0]
+        }
+    }
+    return true
+}
+
+
+
+//we need a message/prompt when its a players turn/the game is won etc
+const message = document.createElement('h2')
+//created a message here and can add content and styles to it:
+message.textContent = "X's turn!"
+message.style.marginTop ="30px"
+message.style.textAlign ="center"
+board.after(message)
+//need to look into this more
+
