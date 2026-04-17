5af1836c65aba4f65f7479b7c6514e1b0ddaf1a2
bibipearce <b.pearce@corecomtechacademy.co.uk>
2026-04-17 01:21:31 +0100

    added some notation to explain logic

--- OandX.js
+++ OandX.js
@@ -8,23 +8,25 @@ const player2 = document.getElementById('player2')
 
 //make players to interact with the game/board
 const players = ['X', 'O']
-let currentPlayer = players[0]
+let currentPlayer = players[0] //first player is X
 //allow players to enter names for personalisation
 function playersNames () {
     if (currentPlayer === 'X') {
-        return player1.value || 'Player 1'
+        return player1.value || 'Player 1' //need to keep 'or player 1' incase no name is entered
     } else {
         return player2.value || 'Player 2'
     }
 }
 
 //we need to define how the game can be won and check for that
+//these are combos of squares that will equate a win
 const winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ]
 
+//this is saying if the text in each box of a winning combo is all X or O, then that player wins
 function checkWin(currentPlayer) {
     for (let i=0; i<winningCombos.length; i++) {
         const [a, b, c] = winningCombos[i]
@@ -38,6 +40,7 @@ function checkWin(currentPlayer) {
     return null
 }
 
+//this is checking if every square is full (with anything)
 function checkTie() {
     for (let i=0; i<squares.length; i++) {
         if (squares[i].textContent === '') {
@@ -50,16 +53,17 @@ function checkTie() {
 let gameActive = true //this will later allow the game to be played or not
 
 //this is what happens when we want to restart
+//clears board and highlighted squares (if any) and resets
 function restart() {
     for (let i=0; i<squares.length; i++) {
         squares[i].textContent = "";
         squares[i].classList.remove('winner');
     }
-    message.textContent = `It is ${playersNames()}'s turn!`;
+    message.textContent = `It is ${playersNames()}'s turn!`; //have to double click button to get correct player message ?? needs fixing
     currentPlayer = players[0];
     gameActive = true;
 }
-restartButton.addEventListener('click', restart)
+restartButton.addEventListener('click', restart) //allows us to execute that function when button is clicked
 
 
 //we need a message/prompt when its a players turn/the game is won etc
@@ -69,37 +73,37 @@ message.textContent = `It is ${playersNames()}'s turn!`
 message.style.marginTop ="30px"
 message.style.textAlign ="center"
 board.after(message)
-//need to look into this more
+//need to look into this more as not sure why created here and not html/css
 
 
-//logic to add all this together and create correct message
-//loop so win/ties are continously looked for
+//logic to add all functions etc above together and create correct messages
+//loop so win/ties are continously looked for each go
 for(let i = 0; i < squares.length; i++){
-    squares[i].addEventListener('click', () => {
-        if (!gameActive) return
+    squares[i].addEventListener('click', () => { //when squares are clicked...
+        if (!gameActive) return //if game is won, exit loop so board is no longer interactive
         
-        if(squares[i].textContent !== ''){
+        if(squares[i].textContent !== ''){ //doesn't allow us to use squares already used
             return
         }
-        squares[i].textContent = currentPlayer
+        squares[i].textContent = currentPlayer //put current players symbol in square
         
         if(checkWin(currentPlayer)) {
             message.textContent=`Game over! ${playersNames()} wins!`
         
             checkWin(currentPlayer).forEach(a => {
-            squares[a].classList.add('winner')
+            squares[a].classList.add('winner') //highlights winning combo of squares using winner from css
         })
             
-        gameActive = false
+        gameActive = false //game no longer active after a win
             return
         }
         
         if(checkTie()) {
             message.textContent= `Game is tied!`
-            gameActive = false
+            gameActive = false 
             return
         }
         currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0] 
-        message.textContent = `It is ${playersNames()}'s turn!`
+        message.textContent = `It is ${playersNames()}'s turn!` //switches between players in the message, whilst game is active
     })   
 }
